82da8182a4dd138b73fdc2f1527fd5331d1e85e3
expiteRz <selfya56223@gmail.com>
2020-10-17 14:04:36 +0900

    updated echo

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "WARNING! WARNING! WARNING!"
 echo "While this script may continue to work, I have deemed it DEPRECIATED."
-echo "If you wish to continue to receive support for a DWC installer, please see: https://github.com/kyle95wm/CoWFC"
+echo "If you wish to continue to receive support for a DWC installer, please see: https://github.com/EnergyCube/CoWFC"
 read -rp "If you understand the risks of using this script, please press the ENTER key. Otherwise please press ctrl+c to exit."
 # DWC Network Installer script by kyle95wm/beanjr
 # NOTE TO DEVELOPERS: please remember to edit the test build section of this script if you have made any changes.
